334f14bbeefac65bc79562ddccacc2eac252d78c
saikyo219 <k.higuchi.219@gmail.com>
2025-01-19 02:57:41 +0900

    fix: url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
-  site: 'https://5315cdac.mokuseinopage.pages.dev',
-  output: 'static',
+  site: "https://26b77a33.mokuseinopage.pages.dev",
+  output: "static",
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
-  }
+  },
 });
